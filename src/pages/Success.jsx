fd49529749fd11fe5d3d2c9fe1ec8229fd4accfd
melisa-kabasakal <meliskabasakal33@gmail.com>
2024-12-03 12:16:27 +0300

    success done

---
+++ src/pages/Success.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../css/Success.css"
+
+function Success() {
+  return (
+    <div className='success'>
+      <img className='img-success' src="./images/iteration-1-images/logo.svg" />
+      <div className='text-success'>
+      <p>TEBRİKLER!</p>
+      <p>SİPARİŞİNİZ ALINDI!</p>
+      </div>
+    </div>
+  )
+}
+
+export default Success
