0d1a71531dff770d88142719aae5c3e72ef053da
melisa-kabasakal <meliskabasakal33@gmail.com>
2024-12-04 19:26:00 +0300

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,44 +1,78 @@
 import React from 'react'
 import styled from 'styled-components'
 //"./images/iteration-1-images/logo.svg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquareTwitter } from '@fortawesome/free-brands-svg-icons';
+
 
 const FooterContainer = styled.footer`
    display: flex;
+   justify-content: space-around;
    align-items: center;
    background-color: #292929;
    color:white;
+   height: 300px;
    
 `
-const FooterTitle = styled.img`
-    max-width: 20px;
-    height: 10px;
+const FooterBottomItem = styled.div`
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
+   flex-direction: row;
+   background-color: #292929;
+   color:white;
+   padding: 0 8rem;
 
 `
+const FooterTitle = styled.img`
+padding-bottom: 2rem;
+`
+
 
 const AddressSection = styled.section`
 
 `
 const AddressInfo = styled.div`
+padding-bottom: 1rem;
 
 `
 const AddressIcon = styled.img`
+   width: 28px;
+   height: 28px;
 
 `
 const FooterList = styled.div`
 
 `
 const FooterListTitle = styled.h3`
+padding-bottom: 10px;
+margin-top: 15px;
 
 `
 
 const FooterUl = styled.ul`
 
 `
-const Listİtem = styled.li`
-   list-style-type: none
+const ListItem = styled.li`
+   list-style-type: none;
+   padding-bottom: 7px;
 
 `
 const InstContainer = styled.div`
+
+`
+const InstImgContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    border: 2px solid #FDC913;
+    border:none;
+    max-width: 330px;
+    max-height: 200px;
+    overflow: hidden;
+    gap:0.5rem;
+
+`
+const InstTitle = styled.h3`
 
 `
 const InstImg = styled.img`
@@ -56,39 +90,46 @@ function Footer() {
     <FooterContainer>
         
         <AddressSection>
-            <FooterTitle src='./images/iteration-2-images/logo-footer.svg'/>
+            <FooterTitle src='./images/iteration-2-images/footer/logo-footer.svg'/>
             <AddressInfo>
-                <AddressIcon src='./images/iteration-2-images/icons/icon-1.png'/><span>341 Londonderry Road,Istanbul Türkiye</span>
+                <AddressIcon src='./images/iteration-2-images/footer/icons/icon-1.png'/><span> 341 Londonderry Road,Istanbul Türkiye</span>
             </AddressInfo>
             <AddressInfo>
-                <AddressIcon src='./images/iteration-2-images/footer/icons/icon-2.png'/><span>aciktim@teknolojikyemekler.com</span>
+                <AddressIcon src='./images/iteration-2-images/footer/icons/icon-2.png'/><span> aciktim@teknolojikyemekler.com</span>
             </AddressInfo>
             <AddressInfo>
-                <AddressIcon src='./images/iteration-2-images/footer/icons/icon-3.png'/><span>+90 216 123 45 67</span>
+                <AddressIcon src='./images/iteration-2-images/footer/icons/icon-3.png'/><span> +90 216 123 45 67</span>
             </AddressInfo>
         </AddressSection>
         <FooterList>
             <FooterUl>
                 <FooterListTitle>Hot Menu</FooterListTitle>
-                <Listİtem>Terminal Pizza</Listİtem>
-                <Listİtem>5 Kişilik Hackathon Pizza</Listİtem>
-                <Listİtem>useEffect Tavuklu Pizza</Listİtem>
-                <Listİtem>Beyaz Console Frosty</Listİtem>
-                <Listİtem>Testler Geçti Mutlu Burger</Listİtem>
-                <Listİtem>Position Absolute Acı Burger</Listİtem>
+                <ListItem>Terminal Pizza</ListItem>
+                <ListItem>5 Kişilik Hackathlon Pizza</ListItem>
+                <ListItem>useEffect Tavuklu Pizza</ListItem>
+                <ListItem>Beyaz Console Frosty</ListItem>
+                <ListItem>Testler Geçti Mutlu Burger</ListItem>
+                <ListItem>Position Absolute Acı Burger</ListItem>
             </FooterUl>
         </FooterList>
+        
         <InstContainer>
+           <InstTitle>Instegram</InstTitle>
+           <InstImgContainer>
             <InstImg src='./images/iteration-2-images/footer/insta/li-0.png'/>
             <InstImg src='./images/iteration-2-images/footer/insta/li-1.png'/>
             <InstImg src='./images/iteration-2-images/footer/insta/li-2.png'/>
             <InstImg src='./images/iteration-2-images/footer/insta/li-3.png'/>
             <InstImg src='./images/iteration-2-images/footer/insta/li-4.png'/>
             <InstImg src='./images/iteration-2-images/footer/insta/li-5.png'/>
+            </InstImgContainer>
         </InstContainer>
-
     </FooterContainer>
-    <FooterBottom>© 2023 Teknolojik Yemekler.</FooterBottom>
+    <FooterBottomItem>
+        <FooterBottom>© 2023 Teknolojik Yemekler.</FooterBottom>
+        <FontAwesomeIcon icon={faSquareTwitter}  style={{padding: '10px'}} />
+    </FooterBottomItem>
+
     </>
   )
 }
--- src/pages/OrderPizza.jsx
+++ src/pages/OrderPizza.jsx
@@ -18,8 +18,8 @@ export default function OrderPizza() {
         Frontend Dev olarak hala position absolute kullanıyorsan bu çok acı
         pizza tam sana göre.Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış,daha sonra odun ateşinde bir fırında yüksek sıcaklıkta pişirilen,genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir...Küçük bir pizzaya bazen pizzatta denir.
       </p>
-      <OrderPizzaForm/>
     </section>
+    <OrderPizzaForm/>
     
   </main>
 </div>
