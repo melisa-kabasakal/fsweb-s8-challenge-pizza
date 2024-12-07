46d0d360bb706cc15f3b75c7f98ff976a13394db
melisa-kabasakal <meliskabasakal33@gmail.com>
2024-12-07 16:52:34 +0300

    app css update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "../css/Home.css"
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
+import { useHistory } from 'react-router-dom'
 import HomeSub from '../components/HomeSub'
 
 function Home() {
@@ -8,21 +8,23 @@ function Home() {
   const history = useHistory()
 
   const handleClick = () => {
+    console.log('Butona tıklandı!')
     history.push('/orderpizza')
   }
+  
   return (
     <>
     <div className='home'>
-      <img className="img-home" src="./images/iteration-1-images/logo.svg" />
+      <img className="img-home" src="../images/iteration-1-images/logo.svg" />
       <p className='lezzet'>lezzeti kaçırma</p>
       <p className='code'>KOD ACIKTIRIR</p>
       <p className='code'>PİZZA, DOYURUR</p>
-      <input type="button" value="ACIKTIM" id="acıktım" onClick={handleClick}/>
+      <input type="button" value="ACIKTIM" id="acıktım" onClick={handleClick} />
     </div>
      <HomeSub/>
      </>
   )
 }
 
-export default Home
+export default Home;
 
